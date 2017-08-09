1829e9a52006af002c73d7d40648673361944acc
Dimitrios J. Vergados <djvergad@gmail.com>
2017-08-09 17:43:22 +0200

    First run on pod

--- bin/runSimAllCPUs.py
+++ bin/runSimAllCPUs.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/home/dimitriv/local/bin/python
 '''
 \brief Start batch of simulations concurrently.
 Workload is distributed equally among CPU cores.
@@ -9,17 +9,20 @@ import os
 import time
 import math
 import multiprocessing
+import fileinput
 
-MIN_TOTAL_RUNRUNS = 500
+MIN_TOTAL_RUNRUNS = 47 # 94 # 500
 
 def runOneSim(params):
-    (cpuID,numRuns) = params
-    command     = []
-    command    += ['python runSimOneCPU.py']
+    (cpuID,numRuns,host) = params
+    command     = ['ssh {0} "cd $PWD;'.format(host)]
+    command    += ['$HOME/local/bin/python runSimOneCPU.py']
     command    += ['--numRuns {0}'.format(numRuns)]
     command    += ['--cpuID {0}'.format(cpuID)]
+    command    += ['"']
     #command    += ['&']
     command     = ' '.join(command)
+    print "Executing command '{0}'".format(command)
     os.system(command)
 
 def printProgress(num_cpus):
@@ -34,18 +37,35 @@ def printProgress(num_cpus):
             if line.count('ended')==0:
                 allDone = False
         output = '\n'.join(output)
-        os.system('clear')
-        print output
+        # os.system('clear')
+        # print output
+        with open('progress.txt', 'w') as f:
+            f.write(output)
+
         if allDone:
             break
     for cpu in range(num_cpus):
         os.remove('cpu{0}.templog'.format(cpu))
 
+def buildSshParams():
+    result = []
+    i = 0
+    for line in fileinput.input():
+        (cores,host)=line.rstrip().split("/")
+        cores = int(cores) if len(cores) > 0 else 0
+        print "We read {0} {1}".format(cores, host)
+        for j in range(int(cores)):
+            result.append(host)
+    return result
+
+
 if __name__ == '__main__':
     multiprocessing.freeze_support()
-    num_cpus = multiprocessing.cpu_count()
+    ssh_params = buildSshParams()
+    print "The ssh params are {0}".format(ssh_params)
+    num_cpus = len(ssh_params) # multiprocessing.cpu_count()
     runsPerCpu = int(math.ceil(float(MIN_TOTAL_RUNRUNS)/float(num_cpus)))
     pool = multiprocessing.Pool(num_cpus)
-    pool.map_async(runOneSim,[(i,runsPerCpu) for i in range(num_cpus)])
+    pool.map_async(runOneSim,[(i,runsPerCpu,ssh_params[i]) for i in range(num_cpus)])
     printProgress(num_cpus)
     raw_input("Done. Press Enter to close.")
